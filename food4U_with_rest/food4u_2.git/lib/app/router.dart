aa9291252694eba75e69c724315194839c7384ca
Burhanuddin99 <muhdburhanuddin12@gmail.com>
2021-06-16 17:00:15 +0800

    View Sikit

--- food4U_with_rest/food4u_2.git/lib/app/router.dart
+++ food4U_with_rest/food4u_2.git/lib/app/router.dart
@@ -1,5 +1,6 @@
 import 'package:exercise3/models/user.dart';
 import 'package:exercise3/screens/register/register_screen.dart';
+import 'package:exercise3/screens/viewproduct/viewproduct.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/login/login_screen.dart';
@@ -16,6 +17,9 @@ Route<dynamic> createRoute(settings) {
 
     case '/register':
       return RegisterScreen.route();
+
+    case '/viewproduct':
+      return ViewProductScreen.route();
   }
   return null;
 }
---
+++ food4U_with_rest/food4u_2.git/lib/screens/viewproduct/viewproduct.dart
@@ -0,0 +1,13 @@
+import 'package:exercise3/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class ViewProductScreen extends StatelessWidget {
+  static Route route() => MaterialPageRoute(builder: (context) => MainScreen());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
